590bb4d1c6e40babcd7250b1271b497ce6e34cec
siteblia-web <siteblia26@gmail.com>
2026-08-27 22:01:58 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    "No se encontró el elemento root en index.html."
+  );
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
